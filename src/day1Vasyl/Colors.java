a3e7c8cbb3b1a826de11fc063ab67c511c068b88
zeeyoda <ziyoda2000@yahoo.com>
2020-03-11 20:20:27 -0400

    modified version

--- src/day1Vasyl/Colors.java
+++ src/day1Vasyl/Colors.java
@@ -2,6 +2,6 @@ package day1Vasyl;
 
 public class Colors {
     public static void main(String[] args) {
-        String[] colors = {"red", "blue", "gree", "yellow"};
+        String[] colors = {"red", "blue", "gree", "yellow", "pink" };
     }
 }
